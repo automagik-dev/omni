7ec5085a856a8aeb694b2d1ae7aa9ea3706f9c12
Genie <genie@namastex.ai>
2026-03-11 01:49:39 -0300

    chore: disable subject-case commitlint rule for proper nouns
    Commit subjects frequently contain proper nouns (Sentry, SDK, Discord,
    WhatsApp, CLI) which legitimately use uppercase. Disable the subject-case
    rule and ignore merge commits to stop false CI failures.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
-  ignores: [(message: string) => message.startsWith('[skip ci]')],
+  rules: {
+    'subject-case': [0],
+  },
+  ignores: [(message: string) => message.startsWith('[skip ci]'), (message: string) => message.startsWith('Merge')],
 };
